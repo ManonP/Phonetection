7f845cfabda6cb4d5ed07468d4f010e2e570b034
DindieWestWood <drouet.dimitri@gmail.com>
2015-10-26 16:24:37 +0100

    image setting and pin setting refactoring

--- app/src/main/java/preference/AlarmVolumePreference.java
+++ app/src/main/java/preference/AlarmVolumePreference.java
@@ -1,5 +1,6 @@
 package preference;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.SharedPreferences;
@@ -9,6 +10,7 @@ import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.Button;
 import android.widget.SeekBar;
 
 import com.ihm15.project.phonetection.R;
@@ -55,7 +57,6 @@ public class AlarmVolumePreference extends DialogPreference implements
 
             }
         });
-
         super.onBindDialogView(view);
     }
 
@@ -69,6 +70,10 @@ public class AlarmVolumePreference extends DialogPreference implements
         valueToSave = initialValue;
 
         setAlarmVolumeSeekBar(initialValue);
+        Button b = ((AlertDialog)getDialog()).getButton(AlertDialog.BUTTON_POSITIVE);
+        b.setTextColor(getContext().getResources().getColor(R.color.accent));
+        b = ((AlertDialog)getDialog()).getButton(AlertDialog.BUTTON_NEGATIVE);
+        b.setTextColor(getContext().getResources().getColor(R.color.accent));
     }
 
 
