35f174117980e8ec543b05080a88b03a3bf19b18
ManonP <paquereau.emilie.manon@hotmail.fr>
2015-11-07 17:50:13 +0100

    Notification lors du branchement du cable.

--- app/src/main/java/com/ihm15/project/phonetection/CardViewActivity.java
+++ app/src/main/java/com/ihm15/project/phonetection/CardViewActivity.java
@@ -1,7 +1,13 @@
 package com.ihm15.project.phonetection;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,6 +18,8 @@ import dialogs.CustomMessageDialog;
  * Created by Manon on 17/10/2015.
  */
 public class CardViewActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
--- app/src/main/java/receiver/CableBroadcastReceiver.java
+++ app/src/main/java/receiver/CableBroadcastReceiver.java
@@ -1,13 +1,21 @@
 package receiver;
 
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.ihm15.project.phonetection.CardViewActivity;
 import com.ihm15.project.phonetection.Data;
+import com.ihm15.project.phonetection.R;
 
 
 import managers.AlertManager;
@@ -19,6 +27,10 @@ public class CableBroadcastReceiver extends BroadcastReceiver {
 
     private String LOG_TAG_BROADCAST_SERVICE = "CableBroadcastReceiver";
     private AlertManager alertManager;
+
+    public int ID_NOTIFICATION = 0;
+
+
     public CableBroadcastReceiver() {
         alertManager = new AlertManager();
 
@@ -30,6 +42,7 @@ public class CableBroadcastReceiver extends BroadcastReceiver {
         if (intent.getAction().equals("android.intent.action.ACTION_POWER_CONNECTED")) {
             Log.e(LOG_TAG_BROADCAST_SERVICE, "Je suis connecté ! ");
             Toast.makeText(context, "Je suis connecté !", Toast.LENGTH_SHORT).show();
+            activeNotification(context);
         }else if (intent.getAction().equals("android.intent.action.ACTION_POWER_DISCONNECTED")) {
             Log.e(LOG_TAG_BROADCAST_SERVICE,"Je suis déconnecté !");
             Toast.makeText(context,"Je suis deconnecté ! ", Toast.LENGTH_SHORT).show();
@@ -37,5 +50,26 @@ public class CableBroadcastReceiver extends BroadcastReceiver {
                 alertManager.startAlarm();
             }
         }
+    }
+
+    private void activeNotification(Context context) {
+        int icon = R.drawable.ic_phonelink_lock_white_36dp;
+        CharSequence tickerText = "Activate the alarm";
+        long when = System.currentTimeMillis();
+        int currentapiVersion = Build.VERSION.SDK_INT;
+        CharSequence text = "Remember to activate the alarm";
+        CharSequence title = "Phonectection";
+        long time = 30;
+        Notification notification = new Notification(icon, text, time);
+        Intent notificationIntent = new Intent(context,CardViewActivity.class);
+        PendingIntent contentIntent = PendingIntent.getActivity(context,0,notificationIntent,0);
+        NotificationManager manager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context);
+        notification = builder.setContentIntent(contentIntent)
+                .setSmallIcon(icon).setTicker(tickerText).setWhen(when)
+                .setAutoCancel(false).setContentTitle(title)
+                .setContentText(text).build();
+        notification.defaults |= Notification.DEFAULT_SOUND;
+        manager.notify(ID_NOTIFICATION, notification);
     }
 }
