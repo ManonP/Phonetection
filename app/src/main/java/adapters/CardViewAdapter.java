091211a04e5f4be783f594ed4a39a4be506821ce
DindieWestWood <drouet.dimitri@gmail.com>
2015-11-07 10:17:39 +0100

    correction du bug

--- app/src/main/java/adapters/CardViewAdapter.java
+++ app/src/main/java/adapters/CardViewAdapter.java
@@ -1,6 +1,7 @@
 package adapters;
 
 import android.content.Context;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.ihm15.project.phonetection.CardViewObject;
@@ -16,7 +18,7 @@ import com.ihm15.project.phonetection.R;
 
 import java.util.ArrayList;
 
-public class CardViewAdapter extends ArrayAdapter<CardViewObject> {
+public class CardViewAdapter extends RecyclerView.Adapter<CardViewAdapter.ViewHolder> {
 
     private static final int MOTION_MODE_POSITION = 0;
     private static final int CHARGER_MODE_POSITION = 1;
@@ -33,15 +35,74 @@ public class CardViewAdapter extends ArrayAdapter<CardViewObject> {
     private Button simButton;
     private Button smsButton;
 
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        public LinearLayout background;
+        public ImageView image;
+        public TextView title;
+        public TextView description;
+        public Button activateDeactivateButton;
+
+        public ViewHolder(View v) {
+            super(v);
+            background = (LinearLayout) v.findViewById(R.id.card_view_background);
+            image = (ImageView) v.findViewById(R.id.card_view_image);
+            title = (TextView) v.findViewById(R.id.card_view_title);
+            description = (TextView) v.findViewById(R.id.card_view_description);
+            activateDeactivateButton = (Button) v.findViewById(R.id.card_view_activate_deactivate_button);
+        }
+    }
 
     public CardViewAdapter(Context context, View.OnClickListener ocl){
-        super(context, 0);
         mContext = context;
         mDataset = getDataSet();
         this.ocl = ocl;
     }
 
     @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.card_view, parent, false);
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.background.setBackgroundColor(mDataset.get(position).getmColor());
+        holder.image.setImageResource(mDataset.get(position).getmImage());
+        holder.title.setText(mDataset.get(position).getmTitle());
+        holder.description.setText(mDataset.get(position).getmDescription());
+        holder.activateDeactivateButton.setText(mDataset.get(position).getmButtonText());
+        holder.activateDeactivateButton.setOnClickListener(ocl);
+        switch (position){
+            case MOTION_MODE_POSITION:
+                holder.activateDeactivateButton.setTag(Data.MOTION_BUTTON_TAG);
+                motionButton = holder.activateDeactivateButton;
+                break;
+            case CHARGER_MODE_POSITION:
+                holder.activateDeactivateButton.setTag(Data.CHARGER_BUTTON_TAG);
+                chargerButton = holder.activateDeactivateButton;
+                break;
+            case SIM_MODE_POSITION:
+                holder.activateDeactivateButton.setTag(Data.SIM_BUTTON_TAG);
+                simButton = holder.activateDeactivateButton;
+                break;
+            case SMS_MODE_POSITION:
+                holder.activateDeactivateButton.setTag(Data.SMS_BUTTON_TAG);
+                smsButton = holder.activateDeactivateButton;
+                break;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mDataset.size();
+    }
+
+    /*@Override
     public int getCount() {
         return mDataset.size();
     }
@@ -95,7 +156,7 @@ public class CardViewAdapter extends ArrayAdapter<CardViewObject> {
             v = convertView;
         }
         return v;
-    }
+    }*/
 
     private ArrayList<CardViewObject> getDataSet() {
         ArrayList al = new ArrayList<>();
